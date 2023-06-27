513225f78caaf61bf761629a8075b7b6163b4724
howruz <120146507+howruz@users.noreply.github.com>
2023-06-27 09:37:46 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int i=0; i<array.length; i++) {
+            if(array[i] > height){
+                answer = array.length - i;
+                break;
+            }else{
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
