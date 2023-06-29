15f5d3482497170880cd7b6467a1fb8237837cc0
howruz <120146507+howruz@users.noreply.github.com>
2023-06-29 09:09:36 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,15 +1,16 @@
 -- 코드를 입력하세요
 SELECT 
-    USER_ID 
-    , NICKNAME 
-    , CITY || ' ' || STREET_ADDRESS1 || ' ' || STREET_ADDRESS2 AS 전체주소 
-    , REGEXP_REPLACE(TLNO, '(.{3})(.{4})(.{4})', '\1-\2-\3') AS 전화번호 
-FROM USED_GOODS_USER 
-WHERE USER_ID IN (
-                    SELECT 
-                        WRITER_ID 
-                    FROM USED_GOODS_BOARD 
-                    GROUP BY WRITER_ID 
-                    HAVING COUNT(WRITER_ID) >= 3 
-                    )
-ORDER BY USER_ID DESC;
+    U.USER_ID 
+    , U.NICKNAME 
+    , U.CITY || ' ' || U.STREET_ADDRESS1 || ' ' || U.STREET_ADDRESS2 AS "전체주소" 
+    , REGEXP_REPLACE(U.TLNO, '(.{3})(.+)(.{4})', '\1-\2-\3') AS "전화번호" 
+FROM USED_GOODS_USER U 
+JOIN (
+    SELECT 
+        WRITER_ID 
+    FROM USED_GOODS_BOARD 
+    GROUP BY WRITER_ID 
+    HAVING COUNT(BOARD_ID) >= 3 
+) B 
+ON U.USER_ID = B.WRITER_ID 
+ORDER BY U.USER_ID DESC;
