aaafdf8cca6fe90fe1a0371278c81890262efac9
howruz <120146507+howruz@users.noreply.github.com>
2023-07-31 10:18:27 +0900

    [Silver IV] Title: 덱, Time: 208 ms, Memory: 18928 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		Deque<Integer> deque = new ArrayDeque<>();
+		StringBuilder sb = new StringBuilder();
+		
+		while(N-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			
+			switch(st.nextToken()) {
+				case "push_front":
+					deque.addFirst(Integer.parseInt(st.nextToken()));
+					break;
+				
+				case "push_back":
+					deque.addLast(Integer.parseInt(st.nextToken()));
+					break;
+					
+				case "pop_front":
+					if(deque.peekFirst() == null) {
+						sb.append(-1).append("\n");
+						break;
+					}
+					
+					sb.append(deque.peekFirst()).append("\n");
+					deque.poll();
+					break;
+					
+					
+				case "pop_back":
+					if(deque.peekLast() == null) {
+						sb.append(-1).append("\n");
+						break;
+					}
+					
+					sb.append(deque.peekLast()).append("\n");
+					deque.pollLast();
+					break;
+					
+				case "size":
+					sb.append(deque.size()).append("\n");
+					break;
+					
+				case "empty":
+					if(deque.isEmpty()) {
+						sb.append(1).append("\n");
+						break;
+					}else {
+						sb.append(0).append("\n");
+						break;
+					}
+					
+				case "front":
+					if(deque.peekFirst() == null) {
+						sb.append(-1).append("\n");
+						break;
+					}else {
+						sb.append(deque.peekFirst()).append("\n");
+						break;
+					}
+					
+				case "back":
+					if(deque.peekLast() == null) {
+						sb.append(-1).append("\n");
+						break;
+					}else {
+						sb.append(deque.peekLast()).append("\n");
+						break;
+					}
+			}
+		}
+		
+		System.out.print(sb);
+
+	}
+
+}
