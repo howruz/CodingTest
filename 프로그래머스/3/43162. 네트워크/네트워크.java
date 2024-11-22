ed3eaeb8a39efc7f39c0530d6b16cf3161af2076
howruz <120146507+howruz@users.noreply.github.com>
2024-11-22 16:47:18 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,26 @@
+class Solution {
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for (int i = 0; i < computers.length; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i, computers);
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void dfs(int node, int[][] computers) {
+        visited[node] = true;
+        
+        for (int i = 0; i < computers[node].length; i++) {
+            if (!visited[i] && computers[node][i] == 1) {
+                dfs(i, computers);
+            }
+        }
+    }
+}
